202a6cbd2a4e5765ef5e685789907b1c34fd7538
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-01-23 14:43:49 +0100

    comp time done

--- vadzim/ngApp/src/app/labsmd/labsmd.module.ts
+++ vadzim/ngApp/src/app/labsmd/labsmd.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LabsoverviewComponent } from './labsoverview/labsoverview.component';
+import { TimeComponent } from './time/time.component';
+import { DiceComponent } from './dice/dice.component';
 
 
 
 @NgModule({
-  declarations: [LabsoverviewComponent],
+  declarations: [
+    LabsoverviewComponent,
+    TimeComponent,
+    DiceComponent
+  ],
   imports: [
     CommonModule
   ],
