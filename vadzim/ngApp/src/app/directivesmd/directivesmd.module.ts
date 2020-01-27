1936198f3e800e85e8f67e0b321c81053cb5a917
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-01-27 19:57:28 +0100

    ng g d directivesmd/test1selector --skipTests

--- vadzim/ngApp/src/app/directivesmd/directivesmd.module.ts
+++ vadzim/ngApp/src/app/directivesmd/directivesmd.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoverviewComponent } from './doverview/doverview.component';
+import { Test1selectorDirective } from './test1selector.directive';
 
 
 
 @NgModule({
-  declarations: [DoverviewComponent],
+  declarations: [DoverviewComponent, Test1selectorDirective],
   imports: [
     CommonModule
   ]
---
+++ vadzim/ngApp/src/app/directivesmd/test1selector.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTest1selector]'
+})
+export class Test1selectorDirective {
+
+  constructor() { }
+
+}
