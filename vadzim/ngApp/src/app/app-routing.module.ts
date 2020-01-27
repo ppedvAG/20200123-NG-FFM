f81d100cc85fb5ca60f5c1f4e216dee702ddbc26
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-01-27 19:48:59 +0100

    doverview comp in die nav eingebunden

--- vadzim/ngApp/src/app/app-routing.module.ts
+++ vadzim/ngApp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LabsoverviewComponent } from './labsmd/labsoverview/labsoverview.component';
 import { TodosoverviewComponent } from './todosmd/todosoverview/todosoverview.component';
 import { AppComponent } from './app.component';
+import { DoverviewComponent } from './directivesmd/doverview/doverview.component';
 
 const routes: Routes = [
   { path: 'labs', component: LabsoverviewComponent},
   { path: 'todos', component: TodosoverviewComponent},
+  { path: 'directives', component: DoverviewComponent},
   { path: '', redirectTo: '/', pathMatch: 'full'},
   { path: '**', redirectTo: '/', pathMatch: 'full'}
 ];
